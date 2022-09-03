526c5cbb3940e623adcde04916d01e34846261b7
hayatul-islam <hayatulemon340@gmail.com>
2022-09-03 19:48:30 +0600

    show transaction

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -1,15 +1,32 @@
+import { useSelector } from "react-redux";
 import Transaction from "./Transaction";
 
 export default function Transactions() {
-    return (
-        <>
-            <p className="second_heading">Your Transactions:</p>
+  const { transactions, isLoading, isError, error } = useSelector(
+    (state) => state.transactions
+  );
+  console.log(transactions);
 
-            <div className="conatiner_of_list_of_transactions">
-                <ul>
-                    <Transaction />
-                </ul>
-            </div>
-        </>
-    );
+  // decite what to render
+  let content = null;
+  if (isLoading) content = <p> Loading...</p>;
+  if (isLoading && isError) content = <p>{error}</p>;
+  if (!isLoading && !isError && transactions.length > 0) {
+    content = transactions.map((transaction) => (
+      <Transaction key={transaction.id} transaction={transaction} />
+    ));
+  }
+  if (!isLoading && !isError && transactions.length === 0) {
+    content = <p>No Transactions found!</p>;
+  }
+
+  return (
+    <>
+      <p className="second_heading">Your Transactions:</p>
+
+      <div className="conatiner_of_list_of_transactions">
+        <ul>{content}</ul>
+      </div>
+    </>
+  );
 }
